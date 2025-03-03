133bf9b240885a634305e4a6df34707846a7e33b
Kyle <kcenatie@asu.edu>
2025-03-02 23:01:55 -0700

    updated main

--- main.py
+++ main.py
@@ -1,57 +1,77 @@
-import os
 import cv2
-import numpy as np
+import os
 import tensorflow as tf
-import csv
-from sklearn.utils import Bunch
 import frameextractor as fe
 import handshape_feature_extractor as hfe
+import csv
+import re as regex
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+class GestureDetail:
+    def __init__(self, gesture_key, gesture_name, output_label):
+        self.gesture_key = gesture_key
+        self.gesture_name = gesture_name
+        self.output_label = output_label
 
 
-def extract_image_feature(location, input_file, mid_frame_counter):
+class GestureFeature:
+    def __init__(self, gesture_detail: GestureDetail, extracted_feature):
+        self.gesture_detail = gesture_detail
+        self.extracted_feature = extracted_feature
 
-    frame_path = fe.frameExtractor(location + input_file, location + "frames/", mid_frame_counter)
-    middle_image = cv2.imread(frame_path, cv2.IMREAD_GRAYSCALE)
-    response = hfe.HandShapeFeatureExtractor.get_instance().extract_feature(middle_image)
+
+def extract_feature(location, input_file, mid_frame_counter):
+    middle_image = cv2.imread(fe.frameExtractor(location + input_file, location + "frames/", mid_frame_counter), cv2.IMREAD_GRAYSCALE)
+    if middle_image is None:
+        return None
+
+    response = hfe.HandShapeFeatureExtractor.extract_feature(hfe.HandShapeFeatureExtractor.get_instance(), middle_image)
     return response
 
 
-def find_gesture_by_filename(gesture_file_name):
-    return next((x for x in gesture_details if x.gesture_key == gesture_file_name.split('_')[0]), None)
+def decide_gesture_by_file_name(gesture_file_name):
+    match = regex.match(r"([A-Za-z0-9]+)_PRACTICE_\d+", gesture_file_name)
+    if match:
+        gesture_key = match.group(1)
+        for x in gesture_details:
+            if x.gesture_key.lower() == gesture_key.lower():
+                return x
+    return None
 
 
-def identify_gesture_from_video(gesture_location, gesture_file_name, mid_frame_counter, featureVectorList):
-    video_feature = extract_image_feature(gesture_location, gesture_file_name, mid_frame_counter)
+def determine_gesture(gesture_location, gesture_file_name, mid_frame_counter):
+    video_feature = extract_feature(gesture_location, gesture_file_name, mid_frame_counter)
 
-    cos_sin, gesture_detail = min(
-        ((tf.keras.losses.cosine_similarity(video_feature, fv.extracted_feature, axis=-1).numpy(), fv.gesture_detail)
-        for fv in featureVectorList),
-        key=lambda x: x[0], default=(1.0, Bunch(gesture_key="", gesture_name="", output_label=""))
-    )
+    if video_feature is None:
+        return None
 
-    print(f"{gesture_file_name} calculated gesture {gesture_detail.gesture_name}")
-    return gesture_detail
+    cos_sin = 1
+    recognized_gesture_detail = None
+
+    for featureVector in featureVectorList:
+        calc_cos_sin = tf.keras.losses.cosine_similarity(
+            video_feature,
+            featureVector.extracted_feature,
+            axis=-1
+        )
+        if calc_cos_sin < cos_sin:
+            cos_sin = calc_cos_sin
+            recognized_gesture_detail = featureVector.gesture_detail
+
+    
+    return recognized_gesture_detail
+
 
 gesture_details = [
-    Bunch(gesture_key="Num0", gesture_name="0", output_label="0"),
-    Bunch(gesture_key="Num1", gesture_name="1", output_label="1"),
-    Bunch(gesture_key="Num2", gesture_name="2", output_label="2"),
-    Bunch(gesture_key="Num3", gesture_name="3", output_label="3"),
-    Bunch(gesture_key="Num4", gesture_name="4", output_label="4"),
-    Bunch(gesture_key="Num5", gesture_name="5", output_label="5"),
-    Bunch(gesture_key="Num6", gesture_name="6", output_label="6"),
-    Bunch(gesture_key="Num7", gesture_name="7", output_label="7"),
-    Bunch(gesture_key="Num8", gesture_name="8", output_label="8"),
-    Bunch(gesture_key="Num9", gesture_name="9", output_label="9"),
-    Bunch(gesture_key="FanDown", gesture_name="Decrease Fan Speed", output_label="10"),
-    Bunch(gesture_key="FanOn", gesture_name="FanOn", output_label="11"),
-    Bunch(gesture_key="FanOff", gesture_name="FanOff", output_label="12"),
-    Bunch(gesture_key="FanUp", gesture_name="Increase Fan Speed", output_label="13"),
-    Bunch(gesture_key="LightOff", gesture_name="LightOff", output_label="14"),
-    Bunch(gesture_key="LightOn", gesture_name="LightOn", output_label="15"),
-    Bunch(gesture_key="SetThermo", gesture_name="SetThermo", output_label="16")
+    GestureDetail("Num0", "0", "0"), GestureDetail("Num1", "1", "1"),
+    GestureDetail("Num2", "2", "2"), GestureDetail("Num3", "3", "3"),
+    GestureDetail("Num4", "4", "4"), GestureDetail("Num5", "5", "5"),
+    GestureDetail("Num6", "6", "6"), GestureDetail("Num7", "7", "7"),
+    GestureDetail("Num8", "8", "8"), GestureDetail("Num9", "9", "9"),
+    GestureDetail("FanDown", "Decrease Fan Speed", "10"),
+    GestureDetail("FanOn", "FanOn", "11"), GestureDetail("FanOff", "FanOff", "12"),
+    GestureDetail("FanUp", "Increase Fan Speed", "13"),
+    GestureDetail("LightOff", "LightOff", "14"), GestureDetail("LightOn", "LightOn", "15"),
+    GestureDetail("SetThermo", "SetThermo", "16")
 ]
 
 featureVectorList = []
@@ -59,20 +79,38 @@ path_to_train_data = "traindata/"
 count = 0
 for file in os.listdir(path_to_train_data):
     if not file.startswith('.') and not file.startswith('frames') and not file.startswith('results'):
-        gesture = find_gesture_by_filename(file)
-        if gesture:
-            featureVectorList.append(Bunch(gesture_detail=gesture, extracted_feature=extract_image_feature(path_to_train_data, file, count)))
-            count += 1
+        gesture_detail = decide_gesture_by_file_name(file)
+        if gesture_detail:
+            feature = extract_feature(path_to_train_data, file, count)
+            if feature is not None:
+                featureVectorList.append(GestureFeature(gesture_detail, feature))
+        count += 1
 
 video_locations = ["test/"]
 test_count = 0
 
-with open('Results.csv', 'w', newline='') as results_file:
-    csv_writer = csv.writer(results_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+video_locations = ["test/"]
+test_count = 0
+max_test_count = 51 
 
+with open('Results.csv', 'w', newline='') as results_file:
+    train_data_writer = csv.writer(results_file)
+
+    test_files = []
     for video_location in video_locations:
         for test_file in os.listdir(video_location):
-            if not test_file.startswith('.') and not test_file.startswith('frames') and not test_file.startswith('results'):
-                recognized_gesture_detail = identify_gesture_from_video(video_location, test_file, test_count, featureVectorList)
-                test_count += 1
-                csv_writer.writerow([recognized_gesture_detail.output_label])
+            if not test_file.startswith('.') and not test_file.startswith('frames') \
+                    and not test_file.startswith('results'):
+                test_files.append(os.path.join(video_location, test_file))
+    
+    test_files = test_files[:max_test_count]
+
+
+    for test_file in test_files:
+        recognized_gesture_detail = determine_gesture(os.path.dirname(test_file) + '/', os.path.basename(test_file), test_count)
+        test_count += 1
+
+        if recognized_gesture_detail:
+            train_data_writer.writerow([recognized_gesture_detail.output_label])
+        else:
+            train_data_writer.writerow(["N/A"])
